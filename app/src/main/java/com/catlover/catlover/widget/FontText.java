68548ceb32aadb9541d7a7118d5b185257f49f22
davidbarbaran <alejandro.david.barbaran@gmail.com>
2017-12-25 21:10:08 -0500

    Add widget custom font

---
+++ app/src/main/java/com/catlover/catlover/widget/FontText.java
@@ -0,0 +1,36 @@
+package com.catlover.catlover.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+import com.catlover.catlover.R;
+
+/**
+ * Created by DAVID on 25/12/2017.
+ */
+
+public class FontText extends AppCompatTextView{
+
+    String mFontPath;
+
+    public FontText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        if (attrs != null) {
+            TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.FontText);
+            mFontPath = typedArray.getString(R.styleable.FontText_font);
+            if (mFontPath != null && !mFontPath.isEmpty()) {
+                Typeface typeface = Typeface.createFromAsset(context.getAssets(), mFontPath);
+                setTypeface(typeface);
+            }
+
+            setTransformationMethod(null);
+        }
+    }
+
+    public FontText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
