e5aad1d054e53b5c4be6acb271315a9e55f2109e
davidbarbaran <alejandro.david.barbaran@gmail.com>
2017-12-27 01:11:57 -0500

    Modify design splash activity

---
+++ app/src/main/java/com/catlover/catlover/activity/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.catlover.catlover.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.catlover.catlover.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
